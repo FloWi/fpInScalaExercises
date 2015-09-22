228874f312b69c530530b5b7f336c758fbf74012
Florian Witteler <florian_witteler@gmx.de>
2015-09-22 16:51:21 +0200

    ex2.1 added tailrec impl of fibonacciTailRec (copied from internet) and tests

--- excercises/src/main/scala/de/flwi/fpInScala/Ex2_1.scala
+++ excercises/src/main/scala/de/flwi/fpInScala/Ex2_1.scala
@@ -1,11 +1,26 @@
 package de.flwi.fpInScala
 
+import scala.annotation.tailrec
+
 object Ex2_1 extends App {
 
   def fibonacci(n: Int): Int = {
     if (n == 1 || n == 2) 1
     else if (n == 0) 0
     else fibonacci(n - 1) + fibonacci(n - 2)
+  }
+
+  /**
+   * by http://peter-braun.org/2012/06/fibonacci-numbers-in-scala/
+   */
+  def fibonacciTailRec(n: Int): Int = {
+
+    @tailrec
+    def fib_tail(n: Int, a: Int, b: Int): Int = n match {
+      case 0 => a
+      case _ => fib_tail(n - 1, b, a + b)
+    }
+    fib_tail(n, 0, 1)
   }
 
 
--- excercises/src/test/scala/Ex2_2Spec.scala
+++ excercises/src/test/scala/Ex2_2Spec.scala
@@ -1,9 +1,40 @@
-import de.flwi.fpInScala.Ex2_2
+import de.flwi.fpInScala.{Ex2_1, Ex2_2}
 import org.scalatest._
+
+class Ex2_1Spec extends FlatSpec with Matchers {
+  "Ex2_1.fibonacci" should "work correctly with the first 15 elements (wikipedia veryfied)" in {
+    import Ex2_1.fibonacci
+    Stream
+      .from(0)
+      .map(fibonacci)
+      .take(15)
+      .toList shouldBe List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377)
+  }
+
+  "Ex2_1.fibonacciTailRec" should "get exactly the same results" in {
+    import Ex2_1._
+    Stream
+      .from(0)
+      .map(n => (fibonacci(n), fibonacciTailRec(n)))
+      .take(40)
+      .toList
+      .forall(t => t._1 == t._2) shouldBe true
+  }
+
+  it should "run reasonably fast" in {
+    import Ex2_1._
+
+    val start = System.currentTimeMillis()
+    fibonacciTailRec(1000)
+    val end = System.currentTimeMillis()
+
+    (end - start) should be < 1000L
+  }
+}
 
 class Ex2_2Spec extends FlatSpec with Matchers {
 
-  "Ex2.2.isSorted" should "detect sorted array correctly" in {
+  "Ex2_2.isSorted" should "detect sorted array correctly" in {
     Ex2_2.isSorted(Array(1, 2, 3, 4), (a: Int, b: Int) => a <= b) shouldBe true
   }
   it should "detect unsorted array correctly" in {
