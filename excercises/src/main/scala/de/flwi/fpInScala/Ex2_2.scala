6e048ca714b2aea887a987e07600bb43f890aff0
Florian Witteler <florian_witteler@gmx.de>
2015-09-22 16:38:34 +0200

    ex2.2 implemented

--- excercises/src/main/scala/de/flwi/fpInScala/Ex2_2.scala
+++ excercises/src/main/scala/de/flwi/fpInScala/Ex2_2.scala
@@ -1,7 +1,28 @@
 package de.flwi.fpInScala
 
+import scala.annotation.tailrec
+
 object Ex2_2 extends App {
 
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+
+    @tailrec
+    def helper(last: A, index: Int): Boolean = {
+
+      if(index >= as.length) true
+      else {
+        val current = as(index)
+        if(ordered(last, current)) helper(current, index+1)
+        else false
+      }
+    }
+
+    as.headOption match {
+      case None => true
+      case Some(head) => helper(head, index = 1)
+    }
+
+  }
 
 
   
---
+++ excercises/src/test/scala/Ex2_2Spec.scala
@@ -0,0 +1,15 @@
+import de.flwi.fpInScala.Ex2_2
+import org.scalatest._
+
+class Ex2_2Spec extends FlatSpec with Matchers {
+
+  "Ex2.2.isSorted" should "detect sorted array correctly" in {
+    Ex2_2.isSorted(Array(1, 2, 3, 4), (a: Int, b: Int) => a <= b) shouldBe true
+  }
+  it should "detect unsorted array correctly" in {
+    Ex2_2.isSorted(Array(2, 1, 3, 4), (a: Int, b: Int) => a <= b) shouldBe false
+  }
+  it should "mark an empty array as sorted" in {
+    Ex2_2.isSorted(Array.empty, (a: Int, b: Int) => a <= b) shouldBe true
+  }
+}
