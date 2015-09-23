8e29bfe84ed960c925632afd262ba597476c216e
Florian Witteler <florian_witteler@gmx.de>
2015-09-23 12:49:17 +0200

    3.24 more elegant from solution

--- excercises/src/main/scala/de/flwi/fpInScala/Ex3_List.scala
+++ excercises/src/main/scala/de/flwi/fpInScala/Ex3_List.scala
@@ -220,5 +220,20 @@ object Ex3_List {
 
       helper(sup, sub)
     }
+
+    @annotation.tailrec
+    def startsWith[A](l: List[A], prefix: List[A]): Boolean = (l,prefix) match {
+      case (_,Nil) => true
+      case (Cons(h,t),Cons(h2,t2)) if h == h2 => startsWith(t, t2)
+      case _ => false
+    }
+
+    @annotation.tailrec
+    def hasSubsequenceFromSol[A](sup: List[A], sub: List[A]): Boolean = sup match {
+      case Nil => sub == Nil
+      case _ if startsWith(sup, sub) => true
+      case Cons(_,t) => hasSubsequenceFromSol(t, sub)
+    }
+
   }
 }
