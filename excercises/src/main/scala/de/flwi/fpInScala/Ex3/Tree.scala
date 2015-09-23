e64db72aa3af2dfe793c5e65615bf7a9b1d5c747
Florian Witteler <florian_witteler@gmx.de>
2015-09-23 15:02:21 +0200

    3.26 done

--- excercises/src/main/scala/de/flwi/fpInScala/Ex3/Tree.scala
+++ excercises/src/main/scala/de/flwi/fpInScala/Ex3/Tree.scala
@@ -2,6 +2,8 @@ package de.flwi.fpInScala.Ex3
 
 sealed trait Tree[+A] {
   def size: Int
+
+
 }
 
 case class Leaf[A](value: A) extends Tree[A] {
@@ -17,5 +19,23 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
 
 object Tree {
 
+
+  def maximum[A: Numeric](tree: Tree[A]): A = {
+
+    val numeric: Numeric[A] = implicitly[Numeric[A]]
+
+    def helper(t: Tree[A], currentMax: A): A = {
+      t match {
+        case Leaf(value) =>
+          numeric.max(value, currentMax)
+        case Branch(left, right) =>
+          val leftMax = helper(left, currentMax)
+          val rightMax = helper(right, currentMax)
+          numeric.max(leftMax, rightMax)
+      }
+    }
+
+    helper(tree, numeric.fromInt(Int.MinValue))
+  }
 
 }
--- excercises/src/test/scala/de/flwi/fpInScala/Ex3_TreeSpec.scala
+++ excercises/src/test/scala/de/flwi/fpInScala/Ex3_TreeSpec.scala
@@ -11,4 +11,9 @@ class Ex3_TreeSpec extends FlatSpec with Matchers {
     Branch(Leaf(1), Leaf(2)).size shouldBe 2
     Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).size shouldBe 3
   }
+
+  "A Tree[Int]" should "find the max value" in {
+    Tree.maximum(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) shouldBe 3
+    Tree.maximum(Branch(Branch(Leaf(-1), Leaf(-2)), Leaf(-3))) shouldBe -1
+  }
 }
