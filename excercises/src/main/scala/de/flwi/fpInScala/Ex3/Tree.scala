00784448ee8b9759978d6813c612eb4496429b0f
Florian Witteler <florian_witteler@gmx.de>
2015-09-23 15:57:00 +0200

    phew 3.29 done

--- excercises/src/main/scala/de/flwi/fpInScala/Ex3/Tree.scala
+++ excercises/src/main/scala/de/flwi/fpInScala/Ex3/Tree.scala
@@ -16,6 +16,20 @@ sealed trait Tree[+A] {
     }
     helper(this)
   }
+
+  def fold[B](l: A => B)(b: (B,B) => B): B = {
+
+    def helper(t: Tree[A]): B = t match {
+      case Leaf(value) => l(value)
+      case Branch(left, right) => b(helper(left), helper(right))
+    }
+
+    helper(this)
+  }
+
+  def foldedSize: Int = fold(_ => 1)(1+_+_)
+  def foldedDepth: Int = fold(_ => 0)((left, right) => 1 + left max right)
+  def foldedMap[B](f: (A) => B): Tree[B] = fold(a => Leaf(f(a)): Tree[B])(Branch(_,_))
 }
 
 case class Leaf[A](value: A) extends Tree[A] {
@@ -25,7 +39,7 @@ case class Leaf[A](value: A) extends Tree[A] {
 
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
 
-  override def size = left.size + right.size
+  override def size = 1 + left.size + right.size
   override def depth = 1 + (left.depth max right.depth)
 }
 
@@ -38,4 +52,7 @@ object Tree {
       case Branch(left, right) => n.max(maximum(left), maximum(right))
     }
   }
+
+  def foldedMaximum[A](tree: Tree[A])(implicit n: Numeric[A]): A = tree.fold(a => a)(n.max)
+
 }
--- excercises/src/test/scala/de/flwi/fpInScala/Ex3_TreeSpec.scala
+++ excercises/src/test/scala/de/flwi/fpInScala/Ex3_TreeSpec.scala
@@ -8,8 +8,8 @@ class Ex3_TreeSpec extends FlatSpec with Matchers {
 
   "A tree" should "calculate the size correctly" in {
     Leaf(1).size shouldBe 1
-    Branch(Leaf(1), Leaf(2)).size shouldBe 2
-    Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).size shouldBe 3
+    Branch(Leaf(1), Leaf(2)).size shouldBe 3
+    Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).size shouldBe 5
   }
 
   it should "calculate the depth correctly" in {
@@ -24,8 +24,34 @@ class Ex3_TreeSpec extends FlatSpec with Matchers {
     Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).map(_*2) shouldBe Branch(Branch(Leaf(2), Leaf(4)), Leaf(6))
   }
 
+  /*
+  Generalize size, maximum, depth, and map, writing a new function fold that abstracts over their similarities.
+  Reimplement them in terms of this more general function.
+ */
+  it should "calculate the foldedSize correctly" in {
+    Leaf(1).foldedSize shouldBe 1
+    Branch(Leaf(1), Leaf(2)).foldedSize shouldBe 3
+    Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).foldedSize shouldBe 5
+  }
+
+  it should "calculate the foldedDepth correctly" in {
+    Leaf(1).foldedDepth shouldBe 0
+    Branch(Leaf(1), Leaf(2)).foldedDepth shouldBe 1
+    Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).foldedDepth shouldBe 2
+  }
+
+  it should "calculate foldedMap correctly" in {
+    Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).foldedMap(_.toString) shouldBe Branch(Branch(Leaf("1"), Leaf("2")), Leaf("3"))
+    Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).foldedMap(_*2) shouldBe Branch(Branch(Leaf(2), Leaf(4)), Leaf(6))
+  }
+
   "A Tree[Int]" should "find the max value" in {
     Tree.maximum(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) shouldBe 3
     Tree.maximum(Branch(Branch(Leaf(-1), Leaf(-2)), Leaf(-3))) shouldBe -1
+  }
+
+  it should "find the foldedMax value" in {
+    Tree.foldedMaximum(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) shouldBe 3
+    Tree.foldedMaximum(Branch(Branch(Leaf(-1), Leaf(-2)), Leaf(-3))) shouldBe -1
   }
 }
