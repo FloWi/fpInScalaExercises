2c741b1fcca0afef942a9ffebaff9c35a1967c61
Florian Witteler <florian_witteler@gmx.de>
2015-09-23 13:04:16 +0200

    3.25 done

--- excercises/src/main/scala/de/flwi/fpInScala/Ex3/Tree.scala
+++ excercises/src/main/scala/de/flwi/fpInScala/Ex3/Tree.scala
@@ -1,14 +1,18 @@
 package de.flwi.fpInScala.Ex3
 
 sealed trait Tree[+A] {
-  def size: Int = {
-    1
-  }
-
+  def size: Int
 }
 
-case class Leaf[A](value: A) extends Tree[A]
-case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+case class Leaf[A](value: A) extends Tree[A] {
+  override def size = 1
+}
+
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
+  override def size = {
+    left.size + right.size
+  }
+}
 
 
 object Tree {
--- excercises/src/test/scala/de/flwi/fpInScala/Ex3_TreeSpec.scala
+++ excercises/src/test/scala/de/flwi/fpInScala/Ex3_TreeSpec.scala
@@ -5,4 +5,10 @@ import org.scalatest.{Matchers, FlatSpec}
 class Ex3_TreeSpec extends FlatSpec with Matchers {
 
   import Ex3._
+
+  "A tree" should "calculate the size correctly" in {
+    Leaf(1).size shouldBe 1
+    Branch(Leaf(1), Leaf(2)).size shouldBe 2
+    Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)).size shouldBe 3
+  }
 }
