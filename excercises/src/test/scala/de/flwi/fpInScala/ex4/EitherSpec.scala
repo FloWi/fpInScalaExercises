fb577293dfe1b715e8eed3b4b5e8843ed5bbda39
Florian Witteler <florian_witteler@gmx.de>
2015-09-24 00:45:18 +0200

    4.6 map correct

--- excercises/src/test/scala/de/flwi/fpInScala/ex4/EitherSpec.scala
+++ excercises/src/test/scala/de/flwi/fpInScala/ex4/EitherSpec.scala
@@ -7,7 +7,19 @@ import scala.{Either => _, Option => _, Some => _} // hide std library `Option`,
 class EitherSpec extends FlatSpec with Matchers {
 
   "Either" should "map correctly" in {
-    Some(1).map(_+1) shouldBe Some(2)
-    (None: Option[Int]).map(_+1) shouldBe None
+    Either.Try(2/0).map(_+2) shouldBe Left(_: Exception)
+    Either.Try(2/2).map(_+2) shouldBe Right(3)
   }
+
+  it should "flatMap correctly" ignore {
+    Some(1).flatMap(i => Some(i+1)) shouldBe Some(2)
+    (None: Option[Int]).flatMap(i => Some(i+1)) shouldBe None
+  }
+
+  it should "orElse correctly" ignore {
+    Some(1).orElse(Some(2)) shouldBe Some(1)
+    (None: Option[Int]).orElse(Some(2)) shouldBe Some(2)
+  }
+
+
 }
