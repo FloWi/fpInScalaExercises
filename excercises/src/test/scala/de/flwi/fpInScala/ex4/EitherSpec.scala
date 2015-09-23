a205e1dbef49e15786d719c6c35e2120f09c6213
Florian Witteler <florian_witteler@gmx.de>
2015-09-24 00:56:11 +0200

    4.6 flatMap correct

--- excercises/src/test/scala/de/flwi/fpInScala/ex4/EitherSpec.scala
+++ excercises/src/test/scala/de/flwi/fpInScala/ex4/EitherSpec.scala
@@ -3,17 +3,22 @@ package de.flwi.fpInScala.ex4
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.{Either => _, Option => _, Some => _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import scala.util.{Try => _}
+import de.flwi.fpInScala.ex4.Either._
 
 class EitherSpec extends FlatSpec with Matchers {
 
+  val failed = Left("exception"): Either[String, Int]
+  val successful = Right(1): Either[String, Int]
+
   "Either" should "map correctly" in {
-    Either.Try(2/0).map(_+2) shouldBe Left(_: Exception)
-    Either.Try(2/2).map(_+2) shouldBe Right(3)
+    failed.map(_+2) shouldBe failed
+    successful.map(_+2) shouldBe Right(3)
   }
 
-  it should "flatMap correctly" ignore {
-    Some(1).flatMap(i => Some(i+1)) shouldBe Some(2)
-    (None: Option[Int]).flatMap(i => Some(i+1)) shouldBe None
+  it should "flatMap correctly" in {
+    failed.flatMap(a => Try(a+1)) shouldBe failed
+    successful.flatMap(a => Try(a+1)) shouldBe Right(2)
   }
 
   it should "orElse correctly" ignore {
