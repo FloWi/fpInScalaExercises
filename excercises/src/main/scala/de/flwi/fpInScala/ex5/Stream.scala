729a5056eed8e89d69bbc75fe7bfed5472dee1d4
Florian Witteler <florian_witteler@gmx.de>
2015-09-24 21:18:55 +0200

    5.2 take and drop done

--- excercises/src/main/scala/de/flwi/fpInScala/ex5/Stream.scala
+++ excercises/src/main/scala/de/flwi/fpInScala/ex5/Stream.scala
@@ -1,5 +1,9 @@
 package de.flwi.fpInScala.ex5
 
+import Stream._
+
+import scala.annotation.tailrec
+
 trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
@@ -27,9 +31,17 @@ trait Stream[+A] {
     helper(this, Nil)
   }
 
-  def take(n: Int): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+}
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  @tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n-1)
+    case _ => this
+  }
 
   def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
 
--- excercises/src/test/scala/de/flwi/fpInScala/ex5/StreamSpec.scala
+++ excercises/src/test/scala/de/flwi/fpInScala/ex5/StreamSpec.scala
@@ -8,4 +8,11 @@ class StreamSpec  extends FlatSpec with Matchers {
     Stream(1,2,3).toList shouldBe List(1,2,3)
     Stream().toList shouldBe Nil
   }
+
+  it should "take(n) correctly" in {
+    Stream(1.to(10): _*).take(3).toList shouldBe List(1, 2, 3)
+  }
+  it should "drop(n) correctly" in {
+    Stream(1.to(10): _*).drop(3).toList shouldBe 4.to(10).toList
+  }
 }
