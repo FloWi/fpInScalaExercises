77d26c882714669e1bb490bb108ec38cec350620
Florian Witteler <florian_witteler@gmx.de>
2015-09-25 14:03:05 +0200

    6.1 nonNegativeInt done

--- excercises/src/main/scala/de/flwi/fpInScala/ex6/State.scala
+++ excercises/src/main/scala/de/flwi/fpInScala/ex6/State.scala
@@ -8,10 +8,10 @@ trait RNG {
 object RNG {
   // NB - this was called SimpleRNG in the book text
 
-  case class Simple(seed: Long) extends RNG {
+  case class SimpleRNG(seed: Long) extends RNG {
     def nextInt: (Int, RNG) = {
       val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
-      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
+      val nextRNG = SimpleRNG(newSeed) // The next state, which is an `RNG` instance created from the new seed.
       val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
       (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
     }
@@ -30,7 +30,14 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val res@(value, nextRNG) = int(rng)
+
+    if(value < 0) {
+      val newValue = if(value == Int.MinValue) Int.MaxValue else -value
+      (newValue, nextRNG)
+    } else res
+  }
 
   def double(rng: RNG): (Double, RNG) = ???
 
--- excercises/src/test/scala/de/flwi/fpInScala/ex6/StateSpec.scala
+++ excercises/src/test/scala/de/flwi/fpInScala/ex6/StateSpec.scala
@@ -1,9 +1,16 @@
 package de.flwi.fpInScala.ex6
 
+import de.flwi.fpInScala.ex6.RNG.SimpleRNG
 import org.scalatest.{FlatSpec, Matchers}
+
+import RNG._
 
 class StateSpec  extends FlatSpec with Matchers {
 
+  "RNG" should "generate nonNegativeInt correclty" in {
+    nonNegativeInt(SimpleRNG(1L))._1 should be >= 0
 
+    1.to(100*1000).map(i => nonNegativeInt(SimpleRNG(i))._1).forall(r => r >= 0) shouldBe true
+  }
 
 }
